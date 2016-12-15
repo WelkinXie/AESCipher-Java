90dd2baf1c2b8885cdb7af7a20cdbade922ebaa2
WelkinXie <welkin995@126.com>
2016-12-16 00:37:48 +0800

    en

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # AESCipher-Java
 
+### AES Encryption For Java
+
+Using this code for __Android__ and __Java back-end__ and [AESCipher-iOS](https://github.com/WelkinXie/AESCipher-iOS) for __iOS__ lets you encrypt and decrypt string with AES accurately.
+
+Encryption:
+
+```
+public static String encryptAES(String content, String key);
+```
+
+Decryption:
+
+```
+public static String decryptAES(String content, String key);
+```
+
+Additionally you can modify the __IV_STRING__ with another 16 bytes string in line 22 of __AESCipher.java__ , but you should make sure to keep it the same with __kInitVector__ in line 14 of [AESCipher-iOS](https://github.com/WelkinXie/AESCipher-iOS).
+
+--
+
 ### 用 Java 实现的 AES 加密。
 
 与 [AESCipher-iOS](https://github.com/WelkinXie/AESCipher-iOS) 一并使用能达到 在iOS、Android、Java后台产生相同密文、正确解密成明文的目的。
@@ -16,6 +36,7 @@ public static String encryptAES(String content, String key);
 public static String decryptAES(String content, String key);
 ```
 
+另外你也可以将 __AESCipher.java__ 第 22 行中的 __IV_STRING__ 修改为其他 16 字节的字符串，但请确保其与 [AESCipher-iOS](https://github.com/WelkinXie/AESCipher-iOS) 第 14 行处的 __kInitVector__ 保持一致。
 
 #### 另附源码详解一份：
 
--- src/AESCipher.java
+++ src/AESCipher.java
@@ -19,7 +19,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class AESCipher {
 
-private static final String IV_STRING = "16-Bytes--String";
+	private static final String IV_STRING = "16-Bytes--String";
 	
 	public static String encryptAES(String content, String key) 
 			throws InvalidKeyException, NoSuchAlgorithmException, 
@@ -28,21 +28,17 @@ private static final String IV_STRING = "16-Bytes--String";
 
 		byte[] byteContent = content.getBytes("UTF-8");
 
-	    // 注意，为了能与 iOS 统一
-	    // 这里的 key 不可以使用 KeyGenerator、SecureRandom、SecretKey 生成
 		byte[] enCodeFormat = key.getBytes();
 	    SecretKeySpec secretKeySpec = new SecretKeySpec(enCodeFormat, "AES");
 			
 	    byte[] initParam = IV_STRING.getBytes();
 	    IvParameterSpec ivParameterSpec = new IvParameterSpec(initParam);
 			
-	    // 指定加密的算法、工作模式和填充方式
 	    Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 	    cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
 		
 	    byte[] encryptedBytes = cipher.doFinal(byteContent);
 		
-	    // 同样对加密后数据进行 base64 编码
 	    Encoder encoder = Base64.getEncoder();
 	    return encoder.encodeToString(encryptedBytes);
 	}
@@ -52,7 +48,6 @@ private static final String IV_STRING = "16-Bytes--String";
 			NoSuchPaddingException, InvalidAlgorithmParameterException, 
 			IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
 			
-	    // base64 解码
 	    Decoder decoder = Base64.getDecoder();
 	    byte[] encryptedBytes = decoder.decode(content);
 		
